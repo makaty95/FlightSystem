96d8917943d0e92c313a794f3de14defa6a8ca7e
Mohammed Metwally <152392304+mohammedetwally@users.noreply.github.com>
2023-12-02 18:36:59 +0200

    SearchFlight and FlightDetails Upgraded

--- src/SearchFlight.java
+++ src/SearchFlight.java
@@ -6,12 +6,18 @@ import java.util.*;
 public class SearchFlight {
 
     public void searchFlight() {
-        FlightDetails[] f = new FlightDetails[5];
-        f[0] = new FlightDetails("101", "Cairo", "Qena", "01:20", "02:10", "First Class  / economy");
-        f[1] = new FlightDetails("102", "Cairo", "America", "05:30", "10:30", "business / economy");
-        f[2] = new FlightDetails("103", "Cairo", "Qena", "01:10", "03:30", "First class / business");
-        f[3] = new FlightDetails("104", "Cairo", "Alex", "01:00", "07:05", "economy / business");
-        f[4] = new FlightDetails("105", "Cairo", "Qena", "01:15", "07:05", "business / First Class");
+
+        ArrayList<FlightDetails> Flights = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++){
+            Flights.add(new FlightDetails());
+        }
+
+        Flights.set(0, new FlightDetails("101", "Cairo", "Qena", "01:20", "02:10", "First Class  / economy"));
+        Flights.set(1,new FlightDetails("102", "Cairo", "America", "05:30", "10:30", "business / economy"));
+        Flights.set(2,new FlightDetails("103", "Cairo", "Qena", "01:10", "03:30", "First class / business"));
+        Flights.set(3,new FlightDetails("104", "Cairo", "Alex", "01:00", "07:05", "economy / business"));
+        Flights.set(4,new FlightDetails("105", "Cairo", "Qena", "01:15", "07:05", "business / First Class"));
 
 
         Scanner In = new Scanner(System.in);
@@ -27,43 +33,63 @@ public class SearchFlight {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM");
         LocalDate today = LocalDate.now();
         int DepartureDay = 0;
+        LocalDate FinalDate = null;
         int check1 = 0;
         do {
+
+            
+
             LocalDate tomorrow = today.plusDays(1);
             LocalDate After2day = today.plusDays(2);
             LocalDate After3day = today.plusDays(3);
             LocalDate After4day = today.plusDays(4);
+            
             System.out.print(1 + "--> ");
             System.out.println(dtf.format(today));
+
             System.out.print(2 + "--> ");
-
             System.out.println(dtf.format(tomorrow));
+
             System.out.print(3 + "--> ");
-
             System.out.println(dtf.format(After2day));
+
             System.out.print(4 + "--> ");
-
             System.out.println(dtf.format(After3day));
-            System.out.print(5 + "--> ");
 
+            System.out.print(5 + "--> ");
             System.out.println(dtf.format(After4day));
+
             System.out.println("Choose Day");
             DepartureDay = In.nextInt();
             if (DepartureDay > 5) {
                 System.out.println("Choose number form 1 to 5");
 
             } else {
-                break;
+                if (DepartureDay == 1)
+                    FinalDate = today;
+
+                else if (DepartureDay == 2)
+                    FinalDate = tomorrow;
+
+                else if (DepartureDay == 3)
+                    FinalDate = After2day;
+
+                else if (DepartureDay == 4)
+                    FinalDate = After3day;
+
+                else if (DepartureDay == 5)
+                    FinalDate = After4day;
             }
+
         } while (DepartureDay > 5);
 
 
         boolean foundFlights = false;
-        FlightDetails[] matchingFlights = new FlightDetails[f.length];
+        FlightDetails[] matchingFlights = new FlightDetails[Flights.size()];
         int count = 0;
-        for (int k = 0; k < f.length; k++) {
-            if (f[k].departureLocation.equals(departureLocationEnter) && f[k].arrivalLocation.equals(arrivalLocationEnter)) {
-                matchingFlights[count] = f[k];
+        for (int k = 0; k < Flights.size(); k++) {
+            if (Flights.get(k).departureLocation.equals(departureLocationEnter) && Flights.get(k).arrivalLocation.equals(arrivalLocationEnter)) {
+                matchingFlights[count] = Flights.get(k);
                 count++;
                 foundFlights = true;
             }
@@ -89,15 +115,13 @@ public class SearchFlight {
             System.out.println("Enter the Flight class: ");
             String FlightClassEnter = In.nextLine();
 
-            for (int i = 0; i < f.length; i++) {
-                if (f[i].flightNum.equals(FlightNumEnter)) {
-                    System.out.println(f[i].flightNum + " | "
-                            + f[i].departureLocation + " | " + f[i].arrivalLocation + " | "
-                            + DepartureDay + " | " + f[i].departure_time + " | " + f[i].arrival_time + " | " + FlightClassEnter);
+            for (int i = 0; i < Flights.size(); i++) {
+                if (Flights.get(i).flightNum.equals(FlightNumEnter)) {
+                    System.out.println(Flights.get(i).flightNum + " | "
+                            + Flights.get(i).departureLocation + " | " + Flights.get(i).arrivalLocation + " | "
+                            + dtf.format(FinalDate) + " | " + Flights.get(i).departure_time + " | " + Flights.get(i).arrival_time + " | " + FlightClassEnter);
                 }
             }
-
-
         }
     }
 }
